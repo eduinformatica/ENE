152a9915cfc45cb09dd1cf2f42e3358ce6cf2f21
Daniel_Gómez_G <dgomez.informatica@gmail.com>
2020-12-01 11:35:24 -0300

    Se cambio el nombre al formulario de Form1 a Login

---
+++ ENE/Login.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ENE
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            RegistroRequerimiento frmRegistroRequerimiento = new RegistroRequerimiento();
+            frmRegistroRequerimiento.ShowDialog();
+        }
+    }
+
+
+}
