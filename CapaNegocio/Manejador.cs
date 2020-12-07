fd0c407206bd01ae32428d98ccfc3443f26cf14a
Daniel_Gómez_G <dgomez.informatica@gmail.com>
2020-12-07 06:22:25 -0300

    Se preparo los procedimientos almacenados con sus respectivos parametros y en el manejador, se creo la conexion a la base de datos mas la ejecucion del procedimiento almacenado

---
+++ CapaNegocio/Manejador.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CapaNegocio
+{
+    public class Manejador
+    {
+        #region Apertura y Cierre de Conexion
+        SqlConnection conexion = new SqlConnection("Server=.;Database=ene; Integrated Security=True");
+
+        private void OpenConexion() {
+            if (conexion.State == ConnectionState.Closed)
+                conexion.Open();
+        }
+
+        private void CloseConexion() {
+            if (conexion.State == ConnectionState.Open)
+                conexion.Close();
+        }
+        #endregion
+
+
+
+
+
+        #region Metodos y Funciones 
+        public void ExecuteStoredProcedure(String nameProcedure, List<Parametros> lst) {
+            SqlCommand cmd;
+
+            try
+            {
+                OpenConexion();
+                cmd = new SqlCommand(nameProcedure, conexion);
+                //Especificamos que es lo que vamos a ejecutar (Stored Procedure)
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                //Validamos que la lista de parametros no venga vacia
+                if (lst != null)
+                {
+                    //Enviamos los datos
+                    for (int i = 0; i < lst.Count; i++)
+                    {
+                        //Direccion de entrada
+                        if (lst[i].directionParameter == ParameterDirection.Input) {
+                            cmd.Parameters.AddWithValue(lst[i].vName, lst[i].vValue);
+                        }
+                        
+                        //Direccion de salida
+                        if (lst[i].directionParameter == ParameterDirection.Output)
+                        {
+                            cmd.Parameters.Add(lst[i].vName, lst[i].typeOfData, lst[i].vSize).Direction = ParameterDirection.Output;
+                        }
+                    }
+                }
+                else {
+                    MessageBox.Show("Error DG-01, No existen argumentos a enviar", "!Aviso ENE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                cmd.ExecuteNonQuery();
+
+                //Recibiendo los datos
+                //Tomamos la respuesta, al parametro vValue, asignale lo que te retorne la respuesta
+                for (int i = 0; i < lst.Count; i++) {
+                    if (cmd.Parameters[i].Direction == ParameterDirection.Output) {
+                        lst[i].vValue = cmd.Parameters[i].Value.ToString();
+                    }
+                }
+
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Error: "+ex, "!Aviso importante", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //throw ex;
+            }
+            CloseConexion();
+        }
+
+
+        public DataTable ListShow(String nameProcedure, List<Parametros> lst) {
+            DataTable dt = new DataTable();
+            SqlDataAdapter dataAdapter;
+
+            try
+            {
+                dataAdapter = new SqlDataAdapter(nameProcedure, conexion);
+                dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                if (lst != null)
+                {
+                    for (int i = 0; i < lst.Count; i++) {
+                        dataAdapter.SelectCommand.Parameters.AddWithValue(lst[i].vName, lst[i].vValue);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Error DG-02, No existen argumentos a enviar", "!Aviso ENE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                dataAdapter.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex, "!Aviso importante", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return dt;
+        }
+        #endregion
+    }
+}
---
+++ CapaNegocio/Parametros.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaNegocio
+{
+    public class Parametros
+    {
+        #region Parametros
+        public String vName { get; set; }
+        public Object vValue { get; set; }
+        public SqlDbType typeOfData { get; set; }
+        public Int32 vSize { get; set; }
+        public ParameterDirection directionParameter { get; set; }
+        #endregion
+
+
+        #region Contructor de Entrada [El que envia los datos] {Envia}
+        public Parametros(String vName, Object vValue) 
+        {
+            this.vName = vName;
+            this.vValue = vValue;
+            this.directionParameter = ParameterDirection.Input;
+        }
+        #endregion
+
+
+        #region Contructores de Salida [El que recibe los datos] {Retorna}
+        public Parametros(String vName, SqlDbType typeOfData, int vSize)
+        {
+            this.vName = vName;
+            this.typeOfData = typeOfData;
+            this.vSize = vSize;
+            this.directionParameter = ParameterDirection.Output;
+        }
+
+        // Otros
+        public Parametros(String vName, Object vValue, SqlDbType typeOfData, ParameterDirection Direccion, int vSize)
+        {
+            this.vName = vName;
+            this.vValue = vValue;
+            this.typeOfData = typeOfData;
+            this.directionParameter = Direccion;
+            this.vSize = vSize;
+        }
+        #endregion
+    }
+}
